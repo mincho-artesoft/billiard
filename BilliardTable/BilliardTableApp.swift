36e3ad944e01814ea2c1729924c4ac4d5b6bf529
Mincho Milev <mincho.milev@gmail.com>
2025-03-01 10:37:12 +0200

    Initial Commit

---
+++ BilliardTable/BilliardTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  BilliardTableApp.swift
+//  BilliardTable
+//
+//  Created by Mincho Milev on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct BilliardTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
